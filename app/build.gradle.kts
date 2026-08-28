7b1d0f130e4147972bf3c5074a27385e63a85b4f
reiangelkay <reiangelkay@gmail.com>
2026-08-28 00:26:11 -0600

    feat: Creación de pantalla de menú principal con logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.fastbite"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.fastbite"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
